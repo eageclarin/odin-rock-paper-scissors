4531b5d90b2b8e72ec1e822f050dd52c9f4cc5ef
Eigram Eclarin <alex.eclarin@j-ports.com>
2025-04-21 22:43:47 +0800

    Added UI and Updated JS

--- script.js
+++ script.js
@@ -8,39 +8,44 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = String(prompt("What's your bet? "))
-    return humanChoice.toLowerCase()
-}
-
 function playRound( humanChoice, computerChoice ) {
     if ( humanChoice === computerChoice ) {
-        return "It's a tie!"
+        document.querySelector('.result').innerHTML = `It's a tie for ${humanChoice} and ${computerChoice}!`
     } else if ( humanChoice === 'rock' && computerChoice === 'paper' 
         || humanChoice === 'paper' && computerChoice === 'scissors'
         || humanChoice === 'scissors' && computerChoice === 'rock'
     ) {
         computerScore++
-        return "Point for computer!"
+        document.querySelector('.computer .score').innerHTML = computerScore
+        document.querySelector('.result').innerHTML = `Point for computer with ${humanChoice} vs ${computerChoice}. Too bad.`
     } else {
         humanScore++
-        return "Point for you!"
+        document.querySelector('.human .score').innerHTML = humanScore
+        document.querySelector('.result').innerHTML = `Point for you with ${humanChoice} vs ${computerChoice}!`
     }
 }
 
-function playGame() {
-    for ( let i=0; i < 5; i++ ) {
-        playRound( getHumanChoice(), getComputerChoice() )
-    }
+function playGame( humanChoice ) {
+    if ( i < 5 ) {
+        playRound( humanChoice, getComputerChoice() )
+        i++
+        document.querySelector('.round span').innerHTML = i
 
-    if ( humanScore > computerScore ) {
-        console.log("You won!")
+        if ( i === 5 ) {
+            playGame( humanChoice )
+        }
+    } else {
+        if ( humanScore > computerScore ) {
+            document.querySelector('.result').innerHTML = "You won, buddy! Nice luck"
+        } else if ( humanScore < computerScore ) {
+            document.querySelector('.result').innerHTML = "You lost, man."
+        } else {
+            document.querySelector('.result').innerHTML = "Oh well, it's a tie. Refresh to try again!"
+        }
     }
-    console.log("You lost.")
 }
 
 let humanScore = 0
 let computerScore = 0
-
-playGame()
+let i = 1
 
